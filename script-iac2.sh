caca1c11b8c67319966bdbf87503cecc3ed1124f
wandersonra <133788763+wandersonra@users.noreply.github.com>
2023-05-16 14:59:24 -0400

    Add files via upload

---
+++ script-iac2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Atualizando o servidor..."
+apt-get update
+apt-get upgrade -y
+
+echo "Instalando o Servidor WEB Apache..."
+apt-get install apache2 -y
+apt-get install unzip -y
+
+
+echo "Baixando e copiando os arquivos da aplicação..."
+
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
+
+
+
